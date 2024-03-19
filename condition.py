c405fbb1aa8a6b3a4af05432254467de99503377
sanskar-decoder <72738470+sanskar-decoder@users.noreply.github.com>
2024-03-19 11:22:43 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,104 @@
+# Conditional Question
+
+'''How we Take input from user '''
+
+'''Age group categorization (less than 13 are child,less than 20 are teen,less than 60 adult,and above 60 are seniors)
+'''
+
+# age=int(input("you age :"))
+# if age<13:
+#     print('Child')
+# elif age<20:
+#     print('Teenager')
+# elif age<60:
+#     print('Adult')
+
+# else:
+#     print('Senior')
+
+
+'''Movie ticket are priced based on age $12 for adult(18 and above) $8 for children Everyone get a $2 discount on wednesday'''
+# age=int(input("Enter your Age \n"))
+# day=str(input("Enter day \n"))
+
+# price=12 if age>=18 else 8
+
+# if day=="wednesday":
+#      price -=2
+#      print("Today Ticket price is :", price)
+
+# else:
+#      print("Today  Ticket price is :",price)
+      
+'''Grade Calculator'''
+
+
+# name=str(input("Name of Student \n"))
+# mark=int(input("Enter your Marks \n"))
+# if mark>=110:
+#     print("Verify grade and put correctly")
+#     exit()
+# if mark>=90 :
+#      print(name,"You Get A Grade")
+# elif mark>= 80:
+#     print(name,"You Get B Grade")
+# elif mark>= 70:
+#     print(name,"You Get C Grade")  
+# elif mark>= 60:
+#     print(name,"You Get D Grade")  
+
+# else :
+#     print(name,"Failed ,better luck next time")
+
+
+'''Fruit Ripness checker'''
+# green-unripe
+#yello-ripe
+#brown-overripe
+
+# color=str(input("what is color of fruit \n"))
+
+# if color=="green":
+#     print("Fruit is unripe")
+# elif color=="yellow":
+#    print("Fruit is ripe")    
+
+# elif color=="brown":
+#    print("Fruit is overripe")    
+
+
+# else :
+#     print("Fruit is defective")   
+
+'''According wheather predict activity'''
+# sunny=go for a walk
+# rainny=read Book
+# snowy = made snowman
+# wheather=str(input("Tell me the Wheather\n"))
+# a=wheather.lower()
+
+# if a=="sunny":
+#     activity="Go for a Walk"
+#     print(activity)
+# elif a=="rainy":
+#     activity="read a book"    
+#     print('activity')
+# elif a=='snowy':
+#     activity="create a snowman"
+#     print('activity')
+# else:
+#     activity="i dont know what activity you want to perform"
+#     print(activity)
+  
+
+'''year is Leap year or Not'''
+
+# year=int(input("Year is :"))
+
+# if (year%400==0) or (year%4==0 and year%100!=0):
+#     data="leap year"
+#     print( year ,"is:", data)
+# else:
+#     data="not leap year"
+#     print(year ,"is",data)
+
